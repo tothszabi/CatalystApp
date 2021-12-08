ef787c8744d270c36ec16c79e6afbafde8cc2382
Szabolcs Toth <szabolcs.toth@bitrise.io>
2021-12-08 16:35:26 +0000

    Initial Commit

---
+++ CatalystApp/CatalystAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalystAppApp.swift
+//  CatalystApp
+//
+//  Created by Szabolcs Toth on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CatalystAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
